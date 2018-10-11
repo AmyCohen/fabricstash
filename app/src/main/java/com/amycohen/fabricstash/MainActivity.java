03244cac547e0caa7c134379f3c6b973f678f8c0
Amy Cohen <anbcohen@gmail.com>
2018-10-10 18:20:44 -0700

    got proof of life for the button, still need to hook up auth to it from Firebase

--- app/src/main/java/com/amycohen/fabricstash/MainActivity.java
+++ app/src/main/java/com/amycohen/fabricstash/MainActivity.java
@@ -1,11 +1,13 @@
 package com.amycohen.fabricstash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+//    @BindView(R.id.LogIn) Button logIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.LogIn)
+    public void logIn() {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
