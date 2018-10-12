9380e5bede3423feabb776ccef75b641b56c7c35
Amy Cohen <anbcohen@gmail.com>
2018-10-11 19:58:50 -0700

    added the create user code from Firebase's quickstart guide. Need to hook up the click and create the form

--- app/src/main/java/com/amycohen/fabricstash/MainActivity.java
+++ app/src/main/java/com/amycohen/fabricstash/MainActivity.java
@@ -63,6 +63,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    //@OnClick(R.id.createAccount)
+//    public void createAccountActivity(){
+//
+//    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+            }
+        });
+    }
+
     @OnClick(R.id.LogIn)
     public void logInActivity () {
         String email = mEmail.getText().toString();
