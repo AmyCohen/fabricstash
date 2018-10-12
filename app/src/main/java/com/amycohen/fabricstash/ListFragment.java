459f137079866a2efd1f6d4ab0ea56e896a25caa
Amy Cohen <anbcohen@gmail.com>
2018-10-11 21:04:14 -0700

    got the basics in place for using a single fragment activity

---
+++ app/src/main/java/com/amycohen/fabricstash/ListFragment.java
@@ -0,0 +1,18 @@
+package com.amycohen.fabricstash;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListFragment extends Fragment {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_list_fragment);
+    }
+}
+
+/*
+public class ErrandListFragment extends Fragment implements ValueEventListener {
+
+ */
