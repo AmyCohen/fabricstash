81d4ce2602631caca4cec95da2659d5ce233862b
Amy Cohen <anbcohen@gmail.com>
2018-10-12 21:03:16 -0700

    adding single item view to the layout for the recycler view.

--- app/src/main/java/com/amycohen/fabricstash/ListFragment.java
+++ app/src/main/java/com/amycohen/fabricstash/ListFragment.java
@@ -1,14 +1,21 @@
 package com.amycohen.fabricstash;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.amycohen.fabricstash.models.Fabric;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +23,9 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ListFragment extends Fragment {
-    //implements ValueEventListener
+public class ListFragment extends Fragment implements ValueEventListener {
     //implements TextWatcher
+    private final String TAG = "DATABASE";
     private List<Fabric> fabrics;
 
     @BindView(R.id.inventoryList) RecyclerView recyclerView;
@@ -32,7 +39,8 @@ public class ListFragment extends Fragment {
 
         ButterKnife.bind(this, view);
 
-
+        DatabaseReference fabricInventoryList = FirebaseDatabase.getInstance().getReference("fabricInventory");
+        fabricInventoryList.addValueEventListener(this);
 
         linearLayoutManager = new LinearLayoutManager(getActivity());
         listAdapter = new ListAdapter(fabrics);
@@ -41,9 +49,18 @@ public class ListFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        // This method is called once with the initial value and again
+        // whenever data at this location is updated.
+        String value = dataSnapshot.getValue(String.class);
+        Log.d(TAG, "Value is: " + value);
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError databaseError) {
+        // Failed to read value
+        Log.w(TAG, "Failed to read value.", databaseError.toException());
+    }
 }
-
-/*
-public class ErrandListFragment extends Fragment implements ValueEventListener {
-
- */
